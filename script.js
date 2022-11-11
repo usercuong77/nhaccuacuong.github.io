f64f6a88bf919c1aa92045b361c07542eec9a690
usercuong77 <111169630+usercuong77@users.noreply.github.com>
2022-11-12 06:50:27 +0700

    Update script.js

--- script.js
+++ script.js
@@ -26,9 +26,9 @@ $(function()
 		url: "Musics/NeuAnhDi.mp3",
 		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
 	}, {
-		artist: "Dig Didzay",
-		name: "NHỮNG BẢN ACOUSTIC COVER HAY NHẤT MỌI THỜI ĐẠI - 60 BÀI HÁT NHẠC TRẺ XƯA 8X 9X HAY NHẤT HIỆN NAY",
-		url: "https://www.youtube.com/watch?v=0lwLgeqaV6M",
+		artist: "Hoài Lâm",
+		name: "Hoa Nở Không Màu - Hoài Lâm - Acoustic Version",
+		url: "Hoa Nở Không Màu - Hoài Lâm - Acoustic Version.mp3",
 		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
 	}];
 	
